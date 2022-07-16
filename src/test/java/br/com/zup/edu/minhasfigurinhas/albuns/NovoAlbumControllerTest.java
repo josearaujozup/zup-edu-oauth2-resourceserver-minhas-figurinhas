c2378d380ffc7e8dd9519347fe1f55ed69ca712a
Denes <jose.araujo@zup.com.br>
2022-07-16 17:13:55 -0300

    com teste spring security

--- src/test/java/br/com/zup/edu/minhasfigurinhas/albuns/NovoAlbumControllerTest.java
+++ src/test/java/br/com/zup/edu/minhasfigurinhas/albuns/NovoAlbumControllerTest.java
@@ -4,10 +4,12 @@ import base.SpringBootIntegrationTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,13 +35,16 @@ class NovoAlbumControllerTest extends SpringBootIntegrationTest {
         );
 
         // ação
-        mockMvc.perform(POST("/api/albuns", novoAlbum))
+        mockMvc.perform(POST("/api/albuns", novoAlbum)
+                        .with(jwt().jwt(jwt -> jwt.claim("preferred_username", "denes"))
+                                .authorities(new SimpleGrantedAuthority("SCOPE_albuns:write"))))
                 .andExpect(status().isCreated())
                 .andExpect(redirectedUrlPattern("**/api/albuns/*"))
                 ;
 
         // validação
         assertEquals(1, repository.count(), "total de albuns");
+        assertEquals("denes", repository.findAll().get(0).getDono());
     }
 
     @Test
@@ -48,12 +53,50 @@ class NovoAlbumControllerTest extends SpringBootIntegrationTest {
         NovoAlbumRequest albumInvalido = new NovoAlbumRequest("", "", null);
 
         // ação
-        mockMvc.perform(POST("/api/albuns", albumInvalido))
+        mockMvc.perform(POST("/api/albuns", albumInvalido)
+                        .with(jwt().authorities(new SimpleGrantedAuthority("SCOPE_albuns:write"))))
                 .andExpect(status().isBadRequest())
         ;
 
         // validação
         assertEquals(0, repository.count(), "total de albuns");
     }
+
+
+    @Test
+    public void naoDeveCadastrarNovoAlbumComSuasFigurinhas_quandoAccessTokenNaoEnviado() throws Exception {
+        // cenário
+        NovoAlbumRequest novoAlbum = new NovoAlbumRequest("CDZ",
+                "Cavaleiros do Zodiaco",
+                List.of(
+                        new NovaFigurinhaRequest("Seya", "http://animes.com/cdz/seya.png"),
+                        new NovaFigurinhaRequest("Hyoga", "http://animes.com/cdz/hyoga.png")
+                )
+        );
+
+        // ação
+        mockMvc.perform(POST("/api/albuns", novoAlbum))
+                .andExpect(status().isUnauthorized())
+        ;
+    }
+
+    @Test
+    public void naoDeveCadastrarNovoAlbumComSuasFigurinhas_quandoAccessTokenNaoPossuiScopeApropriado() throws Exception {
+        // cenário
+        NovoAlbumRequest novoAlbum = new NovoAlbumRequest("CDZ",
+                "Cavaleiros do Zodiaco",
+                List.of(
+                        new NovaFigurinhaRequest("Seya", "http://animes.com/cdz/seya.png"),
+                        new NovaFigurinhaRequest("Hyoga", "http://animes.com/cdz/hyoga.png")
+                )
+        );
+
+        // ação
+        mockMvc.perform(POST("/api/albuns", novoAlbum)
+                        .with(jwt()))
+                .andExpect(status().isForbidden())
+        ;
+    }
+
 
 }
